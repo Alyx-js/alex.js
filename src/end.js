4fa547c760c402e739487cc5e8139cfb09260d4a
User <user@email.com>
2021-03-04 00:27:21 -0700

    Upload

---
+++ src/end.js
@@ -0,0 +1,26 @@
+module.exports = {
+    base: "https://lewds.ga/v1/nsfw/",
+    baseSafe: "https://lewds.ga/api/sfw/",
+    endpoints: {
+        ass: "https://lewds.ga/v1/nsfw/ass",
+        futa: "https://lewds.ga/v1/nsfw/futa",
+        furgif: "https://lewds.ga/v1/nsfw/furgif",
+	furfuta: "https://lewds.ga/v1/nsfw/furfuta",
+        threed: "https://lewds.ga/v1/nsfw/3d",
+        boobs: "https://lewds.ga/v1/nsfw/boobs",
+        athighs: "https://lewds.ga/v1/nsfw/athighs",
+        feet: "https://lewds.ga/v1/nsfw/feet",
+        gifs: "https://lewds.ga/v1/nsfw/gifs",
+		hboobs: "https://lewds.ga/v1/nsfw/hboobs",
+		hentai: "https://lewds.ga/v1/nsfw/hentai",
+		kink: "https://lewds.ga/v1/nsfw/kink",
+		thighs: "https://lewds.ga/v1/nsfw/thighs",
+		yuri: "https://lewds.ga/v1/nsfw/yuri",
+		milk: "https://lewds.ga/v1/nsfw/milk",
+		slime: "https://lewds.ga/v1/nsfw/slime",
+		trap: "https://lewds.ga/v1/nsfw/trap",
+		kiss: "https://lewds.ga/api/sfw/kiss",
+		pantsu: "https://lewds.ga/api/sfw/pantsu",
+		pat: "https://lewds.ga/api/sfw/pat"
+    }
+}
---
+++ src/lewds.js
@@ -0,0 +1,33 @@
+tinees = require('./end');
+fetch = require('node-fetch');
+
+class LewdClient {
+	constructor(options = {}) {
+		if(!options.KEY) throw new Error("No key found for Lewds.GA");
+		this.KEY = options.KEY;
+	};
+    /**
+     * @param {String} img endpoint to search
+     * @returns {String} url of image
+     */
+
+    async nsfw(img) {
+        return fetch(`${tinees.base}${img}?key=${this.KEY}`)
+        .then(res => res.json())
+        .then(json =>  {
+            if(json == undefined) throw Error(`[LEWDS]: ${img} is not a valid endpoint!`)
+            return json.result
+        })
+    }
+
+    async sfw(img) {
+        return fetch(`${tinees.baseSafe}${img}?key=${this.KEY}`)
+        .then(res => res.json())
+        .then(json =>  {
+            if(json.url == undefined) throw Error(`[LEWDS]: ${img} is not a valid endpoint!`)
+            return json.url
+        })
+    }
+}
+
+module.exports = LewdClient
