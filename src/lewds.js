bf31aaca67081a4814cea574649f95315dd4a94e
Lewds Admin <admin@lewds.fun>
2021-06-19 07:40:10 -0400

    Yes

--- src/lewds.js
+++ src/lewds.js
@@ -12,27 +12,13 @@ class LewdClient {
      */
 
     async nsfw(img) {
-        return fetch(`${tinees.base}${img}?key=${this.KEY}`)
+        return fetch(`${tinees.base}${img}`, {
+              method: 'GET',
+              headers: { "Authorization": this.KEY }
+	})
         .then(res => res.json())
         .then(json =>  {
             if(json == undefined) throw Error(`[LEWDS]: ${img} is not a valid endpoint!`)
-            return json.result
-        })
-    }
-
-    async sfw(img) {
-        return fetch(`${tinees.baseSafe}${img}?key=${this.KEY}`)
-        .then(res => res.json())
-        .then(json =>  {
-            if(json.result == undefined) throw Error(`[LEWDS]: ${img} is not a valid endpoint!`)
-            return json.result
-        })
-    }
-    async fun(img) {
-        return fetch(`${tinees.baseFun}${img}?key=${this.KEY}`)
-        .then(res => res.json())
-        .then(json =>  {
-            if(json.result == undefined) throw Error(`[LEWDS]: ${img} is not a valid endpoint!`)
             return json.result
         })
     }
